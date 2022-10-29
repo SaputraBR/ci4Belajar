efd110184d3abf51a53ccfdcb91f00154dea537d
SaputraBR <ahmadbungsu4590@gmail.com>
2022-10-29 14:39:17 +0700

    App.php set base url to heroku app

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://belajar.local/';
+    public $baseURL = 'https://iknowicantmakeyousmile.herokuapp.com/';
 
     /**
      * --------------------------------------------------------------------------
